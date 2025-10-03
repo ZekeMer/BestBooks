c8e5f3f50f4d7dfe71bb5e8ab6ccbb9479f6ae29
Zeke Her <herezem@dunwoody.edu>
2025-10-03 07:51:07 -0500

    New input fields added

--- app.py
+++ app.py
@@ -9,4 +9,19 @@ def mainpage():
 
 @app.route('/book', methods = ['GET', 'POST'])
 def book():
-        return render_template('book.html')
+    if request.method =="POST":
+        name = request.form.get('name', '').strip()
+        title = request.form.get('title', '').strip()
+        author = request.form.get('author', '').strip()
+        rating = request.form.get('rating', '').strip()
+        synopsis = request.form.get('synopsis', '').strip()
+        thoughts = request.form.get('thoughts', '').strip()
+
+        if not name or not rating or not title or not synopsis:
+            msgOfDoom = "Error of Doom! Make sure all required fields are filled in!"
+            return render_template('book.html', error = msgOfDoom)
+
+        # Success! New Bestbook!
+        # return render_template('book.html', name = name, author = author, rating = rating)
+
+    return render_template('book.html')
